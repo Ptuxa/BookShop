023ec0f87fa6e7c6c22e3e31a2a37f0679502f2c
Stanislav Naftolski <89383394+Ptuxa@users.noreply.github.com>
2024-10-21 01:12:38 +0300

    add: books server [PR-1] (#1)
    * feat: add books crud working operations
    
    * feat: add cors
    
    * feat: add bin files to ignore

---
+++ BookShop.Api/DataAccess/BookShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookShop.Api.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Api.DataAccess
+{
+    public class BookShopDbContext : DbContext
+    {
+        public BookShopDbContext(DbContextOptions<BookShopDbContext> options) : base(options) 
+        {
+
+        }
+            
+        public DbSet<BookEntity> Books { get; set; }
+    }
+}
